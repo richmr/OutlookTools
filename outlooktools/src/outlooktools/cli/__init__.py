d0dcf7b8f17bd362334d7f45c35a8d145e676b59
richmr <richmr@users.noreply.github.com>
2023-09-28 17:43:01 -0700

    mailbox list working

--- outlooktools/src/outlooktools/cli/__init__.py
+++ outlooktools/src/outlooktools/cli/__init__.py
@@ -1,17 +1,37 @@
 # SPDX-FileCopyrightText: 2023-present richmr <richmr@users.noreply.github.com>
 #
 # SPDX-License-Identifier: MIT
-import click
 import typer
 
-
 from outlooktools.__about__ import __version__
+from outlooktools.outlook_connection import OutlookConnection
 
 app = typer.Typer(name="OutlookTools")
 
-"""
-@click.group(context_settings={"help_option_names": ["-h", "--help"]}, invoke_without_command=True)
-@click.version_option(version=__version__, prog_name="OutlookTools")
+@app.command()
+def count_items(folder_path:str):
+    """
+    Returns a count of items in the designated folder
+    Expects a folder_path like Inbox/Folder1/Subfolder1 or Inbox\\Folder1\\Subfolder 2
+    """
+    oc = OutlookConnection()
+    folder = oc.traverseFolders(folder_path)
+    messages = list(folder.Items)
+    print(f"There are {len(messages)} messages in {folder_path}")
+    print(f"First subject: {messages[0].Subject}")
+
+@app.command()
+def list_mailboxes():
+    """
+    Prints a list of all available mailboxes for this Outlook instance
+    """
+    oc = OutlookConnection()
+    print("Available mailboxes:")
+    for folder in oc.outlook.Folders:
+        print(f"\t{folder.name}")
+    print("")
+    print("Prepend this to your folder path to delve into correct mailbox")
+
+
 def outlooktools():
-    click.echo("Hello world!")
-"""
+    app()
---
+++ outlooktools/src/outlooktools/outlook_connection.py
@@ -0,0 +1,35 @@
+
+import win32com.client
+import re
+
+class OutlookConnection:
+
+    def __init__(self) -> None:
+        # Initialize connection to the local Outlook instance
+        self.outlook = win32com.client.Dispatch("Outlook.Application").GetNamespace("MAPI")
+        pass
+
+    def getFolder(self, folder_name:str, parent_folder:str = None):
+        folder = None
+        if parent_folder is None:
+            folder = self.outlook.Folders[folder_name]
+        else:
+            folder = parent_folder.Folders[folder_name]
+
+        return folder
+    
+    def getInbox(self):
+        return self.getFolder("Inbox")
+    
+    def traverseFolders(self, folder_path:str):
+        """
+        Will return the final Folder object
+        Expects a folder_path like Inbox/Folder1/Subfolder1 or Inbox\\Folder1\\Subfolder 2
+        Always start with 'Inbox' if that's where the folder is
+        """
+        #  Split the folder path
+        path_list = re.split('[\\\/]', folder_path)
+        folder = self.getFolder(path_list[0])
+        for next_folder in path_list[1:]:
+            folder = self.getFolder(folder_name=next_folder, parent_folder=folder)
+        return folder
